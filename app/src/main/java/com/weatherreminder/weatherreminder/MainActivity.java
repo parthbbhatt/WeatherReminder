822bc3a2967cd87f887c702c9e6f7e4fd79694b3
Parth Brahmbhatt <parth.brahmbhatt2@gmail.com>
2018-06-18 03:03:57 -0400

    Setup the Google api and nav dock

--- app/src/main/java/com/weatherreminder/weatherreminder/MainActivity.java
+++ app/src/main/java/com/weatherreminder/weatherreminder/MainActivity.java
@@ -1,15 +1,22 @@
 package com.weatherreminder.weatherreminder;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +24,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_weather:
+                    mTextMessage.setText(R.string.title_weather);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_map:
+                    mTextMessage.setText(R.string.title_map);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
                     return true;
             }
             return false;
@@ -40,5 +47,29 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+//    public boolean isServicesOk() {
+//        Log.d(TAG, "isServicesOK: checking google services version");
+//        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable
+//                (MainActivity.this);
+//
+//        if(available == ConnectionResult.SUCCESS) {
+//            // no errors detected so user can make requests
+//            Log.d(TAG, "isServicesOK: Google Play Services is working");
+//            return true;
+//
+//        } else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+//            // detected an error but it is resolvable
+//            Log.d(TAG, "isServicesOK: error occurred but it is fixable");
+//            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog
+//                    (MainActivity.this, available, ERROR_DIALOG_REQUEST);
+//            dialog.show();
+//        } else {
+//            Toast.makeText(this, "Map requests cannot be made",
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//
+//        return false;
+//    }
 
 }
